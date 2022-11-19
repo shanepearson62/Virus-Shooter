b1a3df2653f46ac6fcfd5c122984f2a761718d00
Kaitao Chen <79537350+kxc663@users.noreply.github.com>
2022-11-19 00:41:38 -0800

    attempt to implement leaderboard

--- Assets/Scripts/Menu and UI/LeaderBoardController.cs
+++ Assets/Scripts/Menu and UI/LeaderBoardController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using LootLocker.Requests;
 using UnityEngine;
+using TMPro;
 
 public class LeaderBoardController : MonoBehaviour
 {
-    public InputField MemberID, PlayerScore;
     public int ID;
+    public TextMeshProUGUI userID;
+    public TextMeshProUGUI userScore;
+    private bool getData;
 
+    private void Awake(){
+
+    }
     private void Start(){
+        getData = false;
         LootLockerSDKManager.StartSession("virus1", (response)=>
         {
             if(response.success)
@@ -21,8 +28,35 @@ public class LeaderBoardController : MonoBehaviour
             }
         });
     }
+
+
+    private void Update(){
+        if (!getData){
+            LootLockerSDKManager.GetScoreListMain(ID, 5, 0, (response) =>
+            {
+                if(response.success){
+                    string playerNames = "";
+                    string playerScores = "Scores\n";
+
+                    LootLockerLeaderboardMember [] members = response.items;
+
+                    for (int i = 0; i<members.Length; i++){
+                        if (members[i].player.name != ""){
+                            playerNames += members[i].player.name;
+                        }
+                    }
+                    userID.text = playerNames;  
+                }else{
+                    Debug.Log("Failed get data");
+                    userID.text = "12334";
+                }
+                getData = true;
+            });
+        }
+    }
+
     public void SubmitScore(){
-        LootLockerSDKManager.SubmitScore(MemberID.text, HighestScore.currentHighest, ID, (response)=>
+        LootLockerSDKManager.SubmitScore(PlayerPrefs.GetString("ID"), HighestScore.currentHighest, ID, (response)=>
         {
             if(response.success)
             {   
